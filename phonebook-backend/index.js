23d9ab9fdef9bcf5879f1d16c9877a47fe2dbe68
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-05-30 19:08:47 -0300

    Corrigi o erro do Morgan movendo definição de token personalizado para antes do uso.

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,14 +3,14 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.static('dist'))
-
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 // Token personalizado para mostrar o body apenas em requisições POST
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })  
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
