8d1d09646581ae4b4267acf138df654201391945
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-06-03 16:11:36 -0300

    Trouxe a pasta do Notes-backend para cá.

---
+++ Notes-backend/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const Note = require('./models/note')
+
+// Middlewares
+app.use(express.json())
+
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+// Rotas
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if (note) res.json(note)
+      else res.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (req, res, next) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({ error: 'Content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save()
+    .then(savedNote => res.status(201).json(savedNote))
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(error => next(error))
+})
+
+// Middleware para erros
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted ID' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// Inicialização do servidor
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ Notes-backend/models/note.js
@@ -0,0 +1,33 @@
+// Conexão com MongoDB
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+// Definição do Schema e Model
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+// Melhor formatação dos objetos retornados
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
